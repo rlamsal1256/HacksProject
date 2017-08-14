99951ed442ddd5550f65084fef54c5ef528f281b
Andrew Cunningham <andrewcunningham@Andrews-MacBook-Pro.local>
2017-08-14 16:24:22 -0400

    firebase suck

---
+++ app/src/main/java/com/project/libertyhacks/mutual/liberty/care/interfaces/Mapable.java
@@ -0,0 +1,8 @@
+package com.project.libertyhacks.mutual.liberty.care.interfaces;
+
+/**
+ * Created by andrewcunningham on 8/14/17.
+ */
+
+public interface Mapable {
+}
---
+++ app/src/main/java/com/project/libertyhacks/mutual/liberty/care/utilities/FirebaseAccess.java
@@ -0,0 +1,8 @@
+package com.project.libertyhacks.mutual.liberty.care.utilities;
+
+/**
+ * Created by andrewcunningham on 8/14/17.
+ */
+
+public class FirebaseAccess {
+}
