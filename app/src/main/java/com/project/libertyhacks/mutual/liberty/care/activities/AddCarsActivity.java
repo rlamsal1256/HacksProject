180309a10daa0f5255ff4bd882277c22d086574c
Raymond <raymondtang310@gmail.com>
2017-08-15 12:06:34 -0400

    added EnterCarInfo screen

--- app/src/main/java/com/project/libertyhacks/mutual/liberty/care/activities/AddCarsActivity.java
+++ app/src/main/java/com/project/libertyhacks/mutual/liberty/care/activities/AddCarsActivity.java
@@ -1,7 +1,10 @@
 package com.project.libertyhacks.mutual.liberty.care.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.project.libertyhacks.mutual.liberty.care.R;
 
@@ -11,5 +14,14 @@ public class AddCarsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_cars);
+
+        Button addCarBtn = findViewById(R.id.addCarBtn);
+        addCarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddCarsActivity.this, EnterCarInfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
